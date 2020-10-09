055d9d77d249321de6f825e0dd93e7b063d5446f
227Mahaman <zalkiabass.456@gmail.com>
2020-10-09 22:13:02 +0100

    add date_event, time_event and lieu

--- views/view_home.php
+++ views/view_home.php
@@ -79,7 +79,7 @@ ob_start();
                                 <div class="image" style="background-image: url('public/images/img_1.jpg');"></div>
                                 <div class="text">
                                     <h3 class="font-weight-light"><a href="single-post.html"><?= $value['titre']?></a></h3>
-                                    <div class="text-white mb-3"><span class="text-black-opacity-05"><small>By <?= $value['auteur']?> <span class="sep">/</span> <?= $value['date_event']?> <?= $value['time_event']?> <span class="sep">/</span> <?= $value['lieu_event']?></small></span></div>
+                                    <div class="text-white mb-3"><span class="text-black-opacity-05"><small>By <?= $value['auteur']?> <span class="sep">/</span> Date: <?= $value['date_event']?> à <?= $value['time_event']?> <span class="sep">/</span>Lieu: <?= $value['lieu']?></small></span></div>
                                     <p><?= $value['description']?></p>
                                 </div>
                             </div>

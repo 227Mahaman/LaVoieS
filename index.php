f5ed9f73b7a2c64d694b16772187ad967faadaa9
227Mahaman <zalkiabass.456@gmail.com>
2020-10-09 22:28:02 +0100

    update action to p

--- index.php
+++ index.php
@@ -5,17 +5,17 @@ require "model/Manager.php";
 if (isset($_SESSION['messages'])) {
     unset($_SESSION['messages']);
 }
-if (!empty($_GET['action'])) {
+if (!empty($_GET['p'])) {
     extract($_GET);
-    if ($action == "home") {
+    if ($p == "home") {
         include_once('views/view_home.php');
-    } elseif ($action == "fikr") { //View Fikr
+    } elseif ($p == "fikr") { //View Fikr
         include_once('views/view_fikr.php');
-    } elseif ($action == "service") { //View Nos services
+    } elseif ($p == "service") { //View Nos services
         include_once('views/service_view.php');
-    } elseif ($action == "agence") { //View Agence
+    } elseif ($p == "agence") { //View Agence
         include_once('views/agence_view.php');
-    } elseif ($action == "contact") { //View Contact
+    } elseif ($p == "contact") { //View Contact
         if($_POST){//Envoi de mail et notification
             include_once('model/mail_nous_contacter.php');
             if(isset($_SESSION['mail']) && $_SESSION['mail'] == 0){

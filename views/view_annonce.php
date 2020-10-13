18c89b98fee876ebf54f7cea0c6af5f8778fd432
227Mahaman <zalkiabass.456@gmail.com>
2020-10-13 15:42:57 +0100

    View article annonces

--- views/view_annonce.php
+++ views/view_annonce.php
@@ -7,19 +7,19 @@ if ($_SERVER["SERVER_NAME"] == 'localhost') {
 } else {
     $target = "http:///admin/";
 }
-if(isset($_GET['id'])){
-  $id=$_GET['id'];
-  $annonces = Manager::getData("annonces","id", $id)['data'];
-}
+$sql1 = "SELECT COUNT(id) as total FROM annonces WHERE type_annonce='Actualité'";
+$actualites = Manager::getMultiplesRecords($sql1);
+$sql2 = "SELECT COUNT(id) as total FROM annonces WHERE type_annonce='Annonce'";
+$annonces = Manager::getMultiplesRecords($sql2);
 ?>
 
-<div class="site-blocks-cover overlay inner-page-cover" style="background-image: url('<?= $target.Manager::getData("files", "id", $annonces['photo'])['data']['file_url']; ?>');" data-aos="fade" data-stellar-background-ratio="0.5">
+<div class="site-blocks-cover overlay inner-page-cover" style="background-image: url('public/images/voie.jpg');" data-aos="fade" data-stellar-background-ratio="0.5">
       <div class="container">
         <div class="row align-items-center justify-content-center text-center">
 
           <div class="col-md-6" data-aos="fade-up" data-aos-delay="400">
-            <h1 class="text-white"><?= $annonces['titre']?></h1>
-            <a href="index.php?p=home">Accueil</a><span class="mx-2 text-white">&bullet;</span> <span class="text-white"><?= $annonces['date_event']?> à <?= $annonces['time_event']?>.</span>
+            <h1 class="text-white">Annonces</h1>
+            <a href="index.php?p=home">Accueil</a><span class="mx-2 text-white">&bullet;</span> <span class="text-white">Article.</span>
           </div>
         </div>
       </div>
@@ -31,51 +31,66 @@ if(isset($_GET['id'])){
             <div class="row">
                 <div class="col-lg-3">
                     <div class="featured-user  mb-5 mb-lg-0">
-                        <h3 class="mb-4">Actualités</h3>
+                    <h3 class="mb-4">Articles</h3>
                         <ul class="list-unstyled">
                             <li>
                             <a href="#" class="d-flex align-items-center">
                                 <img src="public/images/person_1.jpg" alt="Image" class="img-fluid mr-2">
                                 <div class="podcaster">
-                                <span class="d-block">Claire Stanford</span>
-                                <span class="small">32,420 podcasts</span>
+                                <span class="d-block">Actualité</span>
+                                <span class="small"><?= $actualites['0']['total'];?> articles</span>
                                 </div>
                             </a>
                             </li>
-                        </ul>
-                        <hr>
-                        <h3 class="mb-4">Fikrs</h3>
-                        <ul class="list-unstyled">
-                          <?php
-                          $data = Manager::getData("cfikr", true)['data'];
-                          if (is_array($data) || is_object($data)) {
-                              foreach ($data as $value) {
-                              ?>
+                            <hr>
                             <li>
-                            <a href="index.php?p=fikrs&id=<?= $value['titre']?>" class="d-flex align-items-center">
+                            <a href="#" class="d-flex align-items-center">
                                 <img src="public/images/person_1.jpg" alt="Image" class="img-fluid mr-2">
                                 <div class="podcaster">
-                                <span class="d-block"><?= $value['titre']?></span>
-                                <span class="small">32,420 podcasts</span>
+                                <span class="d-block">Annonces</span>
+                                <span class="small"><?= $annonces['0']['total'];?> articles</span>
                                 </div>
                             </a>
                             </li>
-                            <?php } 
-                              }
-                          ?>
                         </ul>
                     </div>
                 </div>
                 <div class="col-lg-9">
-                  <div class="d-block d-md-flex podcast-entry bg-white mb-5" data-aos="fade-up">
-                      <!--<div class="image" style="background-image: url('<?//= $target.Manager::getData("files", "id", $annonces['photo'])['data']['file_url']; ?>');"></div>-->
-                      <div class="text">
-                          <!--<h3 class="font-weight-light"><a href="index.php?p=annonce&id=<//?= $annonces['id']?>"><?//= $annonces['titre']?></a></h3>-->
-                          <div class="text-white mb-3"><h3><span class="text-black-opacity-05"><small>By <?= $annonces['auteur']?> <span class="sep">/</span>Lieu: <?= $annonces['lieu']?></small></span></h3></div>
-                          <p><?= $annonces['description']?></p>
-                      </div>
-                  </div>
+                    <?php
+                        $data = Manager::getData("annonces", 'type_annonce', 'Annonce', true)['data'];
+                        if (is_array($data) || is_object($data)) {
+                            foreach ($data as $value) {
+                            ?>
+                            <div class="d-block d-md-flex podcast-entry bg-white mb-5" data-aos="fade-up">
+                                <div class="image" style="background-image: url('<?= $target.Manager::getData("files", "id", $value['photo'])['data']['file_url']; ?>');"></div>
+                                <div class="text">
+                                    <h3 class="font-weight-light"><a href="index.php?p=article&id=<?= $value['id']?>"><?= $value['titre']?></a></h3>
+                                    <div class="text-white mb-3"><span class="text-black-opacity-05"><small>By <?= $value['auteur']?> <span class="sep">/</span> Date: <?= $value['date_event']?> à <?= $value['time_event']?> <span class="sep">/</span>Lieu: <?= $value['lieu']?></small></span></div>
+                                    <p><?= $value['description']?></p>
+                                </div>
+                            </div>
+                            <?php } 
+                        }
+                    ?>
                 </div>
+                <div class="container" data-aos="fade-up">
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <div class="site-block-27">
+                                <ul>
+                                    <li><a href="#" class="icon-keyboard_arrow_left"></a></li>
+                                    <li class="active"><span>1</span></li>
+                                    <li><a href="#">2</a></li>
+                                    <li><a href="#">3</a></li>
+                                    <li><a href="#">4</a></li>
+                                    <li><a href="#">5</a></li>
+                                    <li><a href="#" class="icon-keyboard_arrow_right"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     </div>

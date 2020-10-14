b097518c66ec1c397f2b447ae73211c67d4dec32
227Mahaman <zalkiabass.456@gmail.com>
2020-10-14 15:50:23 +0100

    download audio

--- views/view_audio.php
+++ views/view_audio.php
@@ -42,7 +42,7 @@ if ($_SERVER["SERVER_NAME"] == 'localhost') {
                     <div class="image" style="background-image: url('<?= $target.Manager::getData("files", "id", $value['path_url'])['data']['file_url']; ?>');"></div>
                     <div class="text">
                         <h3 class="font-weight-light"><a href="index.php?p=datas&fikr=<?= $value['id']?>"><?= $value['titre']?>, <?= $fikr['titre'];?></a></h3>
-                        <div class="text-white mb-3"><span class="text-black-opacity-05"><small>Publié le <?= $value['date']?></small></span></div>
+                        <div class="text-white mb-3"><span class="text-black-opacity-05"><small>Publié le <?= $value['date']?></small><span class="sep">/</span> <a href="<?= $target.Manager::getData("files", "id", $value['chemin'])['data']['file_url'] ?>" download="<?= $value['titre']?>"><i class="fa fa-car"></i>Télécharger</a></span></div>
                         <div class="player">
                             <audio id="player2" preload="metadata" controls style="max-width: 100%">
                             <source src="<?= $target.Manager::getData("files", "id", $value['chemin'])['data']['file_url'] ?>" type="audio/mp3">

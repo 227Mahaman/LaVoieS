e959e023928a4c38b67a37c0c99b920ec2a6fe97
227Mahaman <zalkiabass.456@gmail.com>
2020-10-15 10:00:45 +0100

    banner image fikr

--- views/view_fikr.php
+++ views/view_fikr.php
@@ -47,16 +47,17 @@ $offset = $perPage * ($currentPage - 1);
  */
 $link = "index.php?p=fikr";
 ?>
-<div class="site-blocks-cover inner-page-cover bg-light mb-5">
+<div class="site-blocks-cover overlay inner-page-cover" style="background-image: url(public/images/voie4.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
   <div class="container">
-    <div class="row align-items-center">
-      <div class="col-12 text-center">
-        <a href="#">Les différents conférences, cours, lectures ...</a><!--<span class="mx-2">&bullet;</span> Jun 25, 2020 <span class="mx-2">&bullet;</span> 1:30:20-->
-        <h1 class="mb-3">FIKRS</h1>
+    <div class="row align-items-center justify-content-center text-center">
+
+      <div class="col-md-6" data-aos="fade-up" data-aos-delay="400">
+        <!--<h1 class="text-white">Au Nom d'ALLAH CLEMENT ET MISERICORDIEUX</h1>-->
+        <a href="index.php?p=home">Accueil</a><span class="mx-2 text-white">&bullet;</span> <span class="text-white">Les différents conférences, cours, lectures ...</span>
       </div>
     </div>
   </div>
-</div>
+</div> 
 <!--<div class="container pt-5 hero">
   <div class="row align-items-center text-center text-md-left">  
   <img src="public/images/voie.jpg" alt="Image" class="img-fluid">
